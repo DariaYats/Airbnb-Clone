04fdc65dcc7f62e2a6d144db94808ad618b501de
Daria Yats <dariayatsyniuk@gmail.com>
2024-03-28 12:03:26 +0100

    Initial Commit

---
+++ AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by Дарья Яцынюк on 28.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
